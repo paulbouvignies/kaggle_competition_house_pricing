d2727674dc35d6563a4611057b299c8ca3551a52
paulbouvignies <38011745+paulbouvignies@users.noreply.github.com>
2022-10-28 10:25:51 +0200

    create submition format function

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import seaborn as sns
+import pandas as pd
 
 # Style for plots
 plt.style.use(style='ggplot')
@@ -15,3 +16,9 @@ def generate_chart(enabled=False, title='title', x=0, y=0, x_label="x_label", y_
         plt.xlabel(x_label)
         plt.ylabel(y_label)
         plt.show()
+
+
+def generate_submission(df_test,prediction):
+    df_submission = pd.DataFrame({'Id': df_test.Id, 'SalePrice': prediction})
+    df_submission.to_csv('sample_submission.csv', index=False)
+    print("Your submission was successfully saved!")
